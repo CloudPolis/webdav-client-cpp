e5114124ce6b426f2f4d33b0cef5ece95ece4ea2
designerror <designerror@yandex.ru>
2015-06-30 22:01:50 +0300

    Update init.cpp

--- examples/client/init.cpp
+++ examples/client/init.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <map>
-#include <string>
-#include <vector>
-#include <client.hpp>
+#include <webdav/client.hpp>
 
 std::map<std::string, std::string> base_options =
         {
