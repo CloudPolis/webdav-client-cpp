9af7b12ff0dad157182dd08069deef353819b71e
designerror <designerror@yandex.ru>
2016-04-06 12:55:33 +0300

    Update fsinfo.cpp

--- source/fsinfo.cpp
+++ source/fsinfo.cpp
@@ -1,30 +1,33 @@
 #include "stdafx.h"
 #include "fsinfo.hpp"
 
-namespace FileInfo
+namespace WebDAV
 {
-	bool exists(std::string & path)
+	namespace FileInfo
 	{
-		std::ifstream file(path);
-		return file.good();
-	}
-
-	std::string name(std::string & path) {
-		std::string filename;
-		std::string separate = "\\";
-		auto separate_position = path.find_last_of(separate);
-		if (separate_position == std::string::npos) return path;
-
-		auto filename_position = separate_position + 1;
-		auto filename_length = path.length() - filename_position;
-		filename = path.substr(filename_position, filename_length);
-
-		return filename;
-	}
-
-	unsigned long long size(std::string & path_file)
-	{
-		std::ifstream file(path_file, std::ios::binary | std::ios::ate);
-		return (unsigned long long)file.tellg();
+		bool exists(std::string & path)
+		{
+			std::ifstream file(path);
+			return file.good();
+		}
+	
+		std::string name(std::string & path) {
+			std::string filename;
+			std::string separate = "\\";
+			auto separate_position = path.find_last_of(separate);
+			if (separate_position == std::string::npos) return path;
+	
+			auto filename_position = separate_position + 1;
+			auto filename_length = path.length() - filename_position;
+			filename = path.substr(filename_position, filename_length);
+	
+			return filename;
+		}
+	
+		unsigned long long size(std::string & path_file)
+		{
+			std::ifstream file(path_file, std::ios::binary | std::ios::ate);
+			return (unsigned long long)file.tellg();
+		}
 	}
 }
