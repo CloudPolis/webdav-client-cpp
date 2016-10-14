ef5fdb3fbb817eae9bfe0e828ed5f6db0ea0985e
designerror <designerror@ya.ru>
2016-10-14 04:09:41 -0600

    adding tools for package building

---
+++ tools/build_package.deb.sh
@@ -0,0 +1,19 @@
+mkdir doc-pak
+cp ../AUTHORS ../LICENSE ./doc-pak
+cp ../README.md ./doc-pak/README
+cp ../DESCRIPTION ./description-pak
+
+checkinstall -y	--type=debian \
+		--install=no \
+		--pkgname=libwdc-dev \
+		--pkgversion=$(git describe --abbrev=0 --tags | sed 's,^v,,') \
+		--pkglicense=MIT \
+		--pkgrelease=1 \
+		--pkgsource="https://github.com/designerror/webdav-client-cpp" \
+		--requires="$(cat ../REQUIREMENTS.UNIX.txt | sed ':a;N;$!ba;s/\n/,/g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | sed 's/>/\\>/g')" \
+		--provides=wdc \
+		--backup=no \
+		--nodoc \
+		--maintainer="designerror@ya.ru" \
+		--pkggroup="wdc"
+
