a7affbd369a932fff6a9fbaf64705845034846d1
designerror <designerror@yandex.ru>
2016-10-16 14:11:12 +0400

    Update build_requirements.win.bat

--- tools/build_requirements.win.bat
+++ tools/build_requirements.win.bat
@@ -1,19 +1,19 @@
-#rem 1. Install [perl](https://www.perl.org/)
-#rem 2. Install [nasm](http://www.nasm.us/)
-#rem 3. Add nasm in environment variable PATH
-#rem 4. Run VS Cross Tools Command Prompt
+;rem 1. Install [perl](https://www.perl.org/)
+;rem 2. Install [nasm](http://www.nasm.us/)
+;rem 3. Add nasm in environment variable PATH
+;rem 4. Run VS Cross Tools Command Prompt
 
-#rem 5. Download repositoty with requirements
-#git clone https://github.com/designerror/webdav-client-cpp
-#cd webdav-client-cpp
-#git submodule update --init
+;rem 5. Download repositoty with requirements
+;git clone https://github.com/designerror/webdav-client-cpp
+;cd webdav-client-cpp
+;git submodule update --init
 
-#rem 6. Set the build options
-#set BUILD_TYPE=Release # Release | Debug
-#set BUILD_SHARED_LIBS=FALSE # FALSE | TRUE
-#set OPENSSL_BUILD_PLATFORM=VC-WIN64A # VC-WIN32 | VC-WIN64A | VC-WIN64I | VC-CE
-#set INSTALL_PREFIX=%cd$\build
-#set CMAKE_COMMON_FLAGS=-DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DMSVC_SHRED_RT:BOOL=%BUILD_SHARED_LIBS%
+;rem 6. Set the build options
+;set BUILD_TYPE=Release # Release | Debug
+;set BUILD_SHARED_LIBS=FALSE # FALSE | TRUE
+;set OPENSSL_BUILD_PLATFORM=VC-WIN64A # VC-WIN32 | VC-WIN64A | VC-WIN64I | VC-CE
+;set INSTALL_PREFIX=%cd$\build
+;set CMAKE_COMMON_FLAGS=-DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DMSVC_SHRED_RT:BOOL=%BUILD_SHARED_LIBS%
 
 rem 7. Build and local install openssl
 cd vendor\openssl/
