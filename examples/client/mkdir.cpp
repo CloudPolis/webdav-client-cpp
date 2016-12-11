ecb760908e9926cbb7c4424fcc440f2c7e46b69d
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-12-11 23:56:12 +0300

    Update mkdir.cpp

--- examples/client/mkdir.cpp
+++ examples/client/mkdir.cpp
@@ -41,13 +41,13 @@ int main() {
 
     for (auto remote_directory : remote_directories) {
         bool is_created = client->create_directory(remote_directory);
-        std::cout << "Directory: " << remote_directory << " is " << is_existed ? "" : "not " << "created" << std::endl;
+        std::cout << "Directory: " << remote_directory << " is " << is_created ? "" : "not " << "created" << std::endl;
     }
 
     auto remote_directory = "not_existing_directory/new_directory";
     bool recursive = true;
     bool is_created = client->create_directory("not_existing_directory/new_directory", recursive);
-    std::cout << "Directory: " << remote_directory << " is " << is_existed ? "" : "not " << "created" << std::endl;
+    std::cout << "Directory: " << remote_directory << " is " << is_created ? "" : "not " << "created" << std::endl;
 }
 
 /// Directory: existing_directory is created
