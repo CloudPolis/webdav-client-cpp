d5a6ca4a26696b8f1f7373a94c10d4e01fa7c9a8
rusdevops <rusdevops@gmail.com>
2019-01-04 09:05:06 +0000

    fixed copy example

--- examples/client/copy.cpp
+++ examples/client/copy.cpp
@@ -24,58 +24,60 @@
 
 #include <memory>
 #include <sstream>
+#include <string>
+#include <vector>
 
-std::string resources_to_string(std::vector<std::string> & resources)
+std::string resources_to_string(const std::vector<std::string>& resources)
 {
-    std::stringstream stream;
-    for (const auto& resource : resources)
-    {
-        stream << "\t" << "- " << resource << std::endl;
-    }
-    return stream.str();
+  std::stringstream result;
+  for (const auto& resource : resources)
+  {
+    result << "\t" << "- " << resource << std::endl;
+  }
+  return result.str();
 }
 
-int main() {
+int main()
+{
+  auto hostname_ptr = std::getenv("WEBDAV_HOSTNAME");
+  auto username_ptr = std::getenv("WEBDAV_USERNAME");
+  auto password_ptr = std::getenv("WEBDAV_PASSWORD");
+  auto root_ptr = std::getenv("WEBDAV_ROOT");
 
-    auto hostname_ptr = std::getenv("WEBDAV_HOSTNAME");
-    auto username_ptr = std::getenv("WEBDAV_USERNAME");
-    auto password_ptr = std::getenv("WEBDAV_PASSWORD");
-    auto root_ptr = std::getenv("WEBDAV_ROOT");
+  if (hostname_ptr == nullptr) return -1;
+  if (username_ptr == nullptr) return -1;
+  if (password_ptr == nullptr) return -1;
 
-    if (hostname_ptr == nullptr) return -1;
-    if (username_ptr == nullptr) return -1;
-    if (password_ptr == nullptr) return -1;
+  std::map<std::string, std::string> options =
+  {
+      { "webdav_hostname", hostname_ptr },
+      { "webdav_username", username_ptr },
+      { "webdav_password", password_ptr }
+  };
 
-    std::map<std::string, std::string> options =
-    {
-        { "webdav_hostname", hostname_ptr },
-        { "webdav_username", username_ptr },
-        { "webdav_password", password_ptr }
-    };
+  if (root_ptr != nullptr) {
+      options["webdav_root"] = root_ptr;
+  }
 
-    if (root_ptr != nullptr) {
-        options["webdav_root"] = root_ptr;
-    }
+  std::unique_ptr<WebDAV::Client> client{ new WebDAV::Client{ options } };
 
-    std::unique_ptr<WebDAV::Client> client{ new WebDAV::Client{ options } };
+  auto remote_file = "file.dat";
+  auto remote_directory = "dir/";
 
-    auto remote_file = "file.dat";
-    auto remote_directory = "dir/";
+  auto copy_remote_file = "file2.dat";
+  auto copy_remote_directory = "dir2/";
 
-    auto copy_remote_file = "file2.dat";
-    auto copy_remote_directory = "dir2/";
+  auto resources = client->list();
+  std::cout << "\"/\" resource contain:" << std::endl;
+  std::cout << resources_to_string(resources) << std::endl;
 
-    auto resources = client->list();
-    std::cout << "\"/\" resource contain:" << std::endl;
-    std::cout << resources_to_string(resources) << std::endl;
+  client->copy(remote_file, copy_remote_file);
+  client->copy(remote_directory, copy_remote_directory);
 
-    client->copy(remote_file, copy_remote_file);
-    client->copy(remote_directory, copy_remote_directory);
+  resources = client->list();
 
-    resources = client->list();
-
-    std::cout << "\"/\" resource contain:" << std::endl;
-    std::cout << resources_to_string(resources) << std::endl;
+  std::cout << "\"/\" resource contain:" << std::endl;
+  std::cout << resources_to_string(resources) << std::endl;
 }
 
 /// "/" resource contain:
